b6a77f05075e2441923bdd652adef153fe030923
javiipzo <91606625+javiipzo@users.noreply.github.com>
2022-11-02 20:23:28 +0100

    listas

---
+++ clase1.py
@@ -0,0 +1,8 @@
+PERSONA_1 = 0; PERSONA_2 = 1; HIJOS = 2; CONVIVENCIA = 3; INGRESOS = 4
+NOMBRE = 0; EDAD = 1; SEXO = 2
+
+parejas = [[['Juan',35,'H'], ['Maria',28,'M'], [['Luis',2,'H'], ['Nuria',4,'M']], 7, 60000],
+           [['Ruth',38,'M'], ['Pablo',35,'H'], [['Alma',8,'M']], 10, 50000],
+           [['Irene',50,'M'], ['Alberto',52,'H'], [['Lucia',27,'M'], ['Carlota',23,'M']], 30, 90000],
+           [['Carla',22,'M'], ['Rocio',24,'M'], [['Thor',1,'H']], 2, 65000],
+           [['Jose',35,'H'], ['Santiago',37,'H'], None, 4, 30000]
